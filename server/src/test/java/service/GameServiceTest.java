a3457ed76395814483d79be7bc8fe3a87382205f
Kitsune2468 <masefro@gmail.com>
2024-10-23 21:27:54 -0600

    All endpoints created and all JUNIT tests passing

---
+++ server/src/test/java/service/GameServiceTest.java
@@ -0,0 +1,136 @@
+package service;
+
+import chess.ChessGame;
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+import org.junit.jupiter.api.*;
+import service.Requests.GameListResult;
+import service.Requests.GameTemplateResult;
+
+import java.util.ArrayList;
+
+public class GameServiceTest {
+    private static GameService controlGameService;
+
+    @BeforeEach
+    public  void createGameService() throws Exception {
+        controlGameService = new GameService(new MemoryGameDAO(), new MemoryAuthDAO());
+    }
+
+    @Test
+    public void testAddGame() throws Exception {
+        MemoryGameDAO testMemoryGameDAO = new MemoryGameDAO();
+        testMemoryGameDAO.addGame("TestGame");
+
+        controlGameService = new GameService(testMemoryGameDAO, new MemoryAuthDAO());
+
+        try {
+            MemoryAuthDAO modifiedAuthDAO = new MemoryAuthDAO();
+            modifiedAuthDAO.addAuth(new AuthData("123", "testUsername"));
+            GameService testGameService = new GameService(new MemoryGameDAO(), modifiedAuthDAO);
+            testGameService.createGame("TestGame","123");
+            Assertions.assertEquals(testGameService, controlGameService);
+        } catch (Exception e) {
+            Assertions.fail();
+        }
+
+    }
+
+    @Test
+    public void testAddExistingGame() throws Exception {
+        MemoryGameDAO testGameDAO = new MemoryGameDAO();
+        testGameDAO.addGame("TestGame");
+        controlGameService = new GameService(testGameDAO, new MemoryAuthDAO());
+
+        try {
+            MemoryAuthDAO modifiedAuthDAO = new MemoryAuthDAO();
+            modifiedAuthDAO.addAuth(new AuthData("123", "testUsername"));
+            GameService testGameService = new GameService(new MemoryGameDAO(), modifiedAuthDAO);
+            testGameService.createGame("TestGame","123");
+            testGameService.createGame("TestGame", "123");
+            Assertions.fail();
+        } catch (DataAccessException e) {
+            Assertions.assertTrue(true);
+        }
+    }
+
+    @Test
+    public void testJoin() throws Exception {
+        MemoryGameDAO testGameDAO = new MemoryGameDAO();
+        testGameDAO.addGame("TestGame");
+        controlGameService = new GameService(testGameDAO, new MemoryAuthDAO());
+
+        try {
+            MemoryAuthDAO modifiedAuthDAO = new MemoryAuthDAO();
+            modifiedAuthDAO.addAuth(new AuthData("123", "testUsername"));
+            GameService testGameService = new GameService(new MemoryGameDAO(), modifiedAuthDAO);
+            testGameService.createGame("TestGame","123");
+            testGameService.joinGame(1,"WHITE","123");
+            Assertions.assertTrue(true);
+        } catch (DataAccessException e) {
+            Assertions.fail();
+        }
+    }
+
+    @Test
+    public void testSameTeamJoin() throws Exception {
+        MemoryGameDAO testGameDAO = new MemoryGameDAO();
+        testGameDAO.addGame("TestGame");
+        controlGameService = new GameService(testGameDAO, new MemoryAuthDAO());
+
+        try {
+            MemoryAuthDAO modifiedAuthDAO = new MemoryAuthDAO();
+            modifiedAuthDAO.addAuth(new AuthData("123", "testUsername"));
+            GameService testGameService = new GameService(new MemoryGameDAO(), modifiedAuthDAO);
+            testGameService.createGame("TestGame","123");
+            testGameService.joinGame(1,"WHITE","123");
+            testGameService.joinGame(1,"WHITE","123");
+            Assertions.fail();
+        } catch (DataAccessException e) {
+            Assertions.assertTrue(true);
+        }
+    }
+
+    @Test
+    public void testListGame() throws Exception {
+        GameTemplateResult testResult = new GameTemplateResult(1, "testUsername", null,  "TestGame");
+        GameListResult testList = new GameListResult(new ArrayList<GameTemplateResult>());
+        testList.games().add(testResult);
+
+        try {
+            MemoryAuthDAO modifiedAuthDAO = new MemoryAuthDAO();
+            modifiedAuthDAO.addAuth(new AuthData("123", "testUsername"));
+            GameService testGameService = new GameService(new MemoryGameDAO(), modifiedAuthDAO);
+            testGameService.createGame("TestGame","123");
+            testGameService.joinGame(1,"WHITE","123");
+            GameListResult checkList = testGameService.listGames("123");
+            Assertions.assertEquals(testList.toString(),checkList.toString());
+        } catch (DataAccessException e) {
+            Assertions.fail();
+        }
+    }
+
+    @Test
+    public void testListGameWrongToken() throws Exception {
+        GameTemplateResult testResult = new GameTemplateResult(1, "testUsername", null,  "TestGame");
+        GameListResult testList = new GameListResult(new ArrayList<GameTemplateResult>());
+        testList.games().add(testResult);
+
+        try {
+            MemoryAuthDAO modifiedAuthDAO = new MemoryAuthDAO();
+            modifiedAuthDAO.addAuth(new AuthData("123", "testUsername"));
+            GameService testGameService = new GameService(new MemoryGameDAO(), modifiedAuthDAO);
+            testGameService.createGame("TestGame","123");
+            testGameService.joinGame(1,"WHITE","123");
+            GameListResult checkList = testGameService.listGames("456");
+            Assertions.fail();
+        } catch (DataAccessException e) {
+            Assertions.assertTrue(true);
+        }
+    }
+}
