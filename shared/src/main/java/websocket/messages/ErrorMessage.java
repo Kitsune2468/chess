43b0f6fbf545eb0d93f0c94cc6a4f6f69ba78ad9
Kitsune2468 <masefro@gmail.com>
2024-12-02 14:29:37 -0700

    Phase 6 - Server Message classes created

---
+++ shared/src/main/java/websocket/messages/ErrorMessage.java
@@ -0,0 +1,44 @@
+package websocket.messages;
+
+import java.util.Objects;
+
+/**
+ * Represents a Message the server can send through a WebSocket
+ * 
+ * Note: You can add to this class, but you should not alter the existing
+ * methods.
+ */
+public class ErrorMessage {
+    ServerMessageType serverMessageType;
+
+    public enum ServerMessageType {
+        LOAD_GAME,
+        ERROR,
+        NOTIFICATION
+    }
+
+    public ErrorMessage(ServerMessageType type) {
+        this.serverMessageType = type;
+    }
+
+    public ServerMessageType getServerMessageType() {
+        return this.serverMessageType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof ErrorMessage)) {
+            return false;
+        }
+        ErrorMessage that = (ErrorMessage) o;
+        return getServerMessageType() == that.getServerMessageType();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getServerMessageType());
+    }
+}
---
+++ shared/src/main/java/websocket/messages/LoadGameMessage.java
@@ -0,0 +1,44 @@
+package websocket.messages;
+
+import java.util.Objects;
+
+/**
+ * Represents a Message the server can send through a WebSocket
+ * 
+ * Note: You can add to this class, but you should not alter the existing
+ * methods.
+ */
+public class LoadGameMessage {
+    ServerMessageType serverMessageType;
+
+    public enum ServerMessageType {
+        LOAD_GAME,
+        ERROR,
+        NOTIFICATION
+    }
+
+    public LoadGameMessage(ServerMessageType type) {
+        this.serverMessageType = type;
+    }
+
+    public ServerMessageType getServerMessageType() {
+        return this.serverMessageType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof LoadGameMessage)) {
+            return false;
+        }
+        LoadGameMessage that = (LoadGameMessage) o;
+        return getServerMessageType() == that.getServerMessageType();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getServerMessageType());
+    }
+}
